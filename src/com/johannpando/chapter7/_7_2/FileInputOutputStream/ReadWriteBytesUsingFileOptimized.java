ac214be73e57d5396ac40d4253e62e13554576ab
Johann <Johann@localhost>
2017-07-08 18:22:02 +0100

    finish push

--- src/com/johannpando/chapter7/_7_2/FileInputOutputStream/ReadWriteBytesUsingFileOptimized.java
+++ src/com/johannpando/chapter7/_7_2/FileInputOutputStream/ReadWriteBytesUsingFileOptimized.java
@@ -1,18 +1,17 @@
 package com.johannpando.chapter7._7_2.FileInputOutputStream;
 
+import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InvalidClassException;
 
 public class ReadWriteBytesUsingFileOptimized {
 
-	public static void main(String[] args) throws FileNotFoundException,
-			IOException {
-		try (FileInputStream fis = new FileInputStream(
-				"c:\\temp\\ocp7\\data\\sample3.pdf");
-				FileOutputStream fos = new FileOutputStream(
-						"c:\\temp\\ocp7\\data\\sample4.pdf")) {
+	public static void main(String[] args) throws IOException {
+		try (FileInputStream fis = new FileInputStream("c:\\temp\\ocp7\\data\\sample3.pdf");
+				FileOutputStream fos = new FileOutputStream("c:\\temp\\ocp7\\data\\sample4.pdf")) {
 			// Declares variable to store a single byte of data
 			int data;
 			// Creates byte array of size 1024
@@ -22,6 +21,8 @@ public class ReadWriteBytesUsingFileOptimized {
 				// Writes only read bytes to output stream
 				fos.write(byteArray, 0, data);
 			}
+		} catch (FileNotFoundException | InvalidClassException | EOFException e) {
+			// TODO: handle exception
 		}
 	}
 }
--- src/com/johannpando/enthuware/threads/FromOneToFive.java
+++ src/com/johannpando/enthuware/threads/FromOneToFive.java
@@ -1,20 +1,39 @@
 package com.johannpando.enthuware.threads;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class FromOneToFive extends Thread {
 
-	private static int threadcounter = 0;
+	// private static volatile int threadcounter = 0;
+
+	private static AtomicInteger threadcounter = new AtomicInteger(0);
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
-			new FromOneToFive().start();
-		}
+		// for (int i = 0; i < 5; i++) {
+		// new FromOneToFive().start();
+		// }
+
+		Thread t1 = new FromOneToFive();
+		Thread t2 = new FromOneToFive();
+		Thread t3 = new FromOneToFive();
+		Thread t4 = new FromOneToFive();
+		Thread t5 = new FromOneToFive();
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
 		System.out.println(threadcounter + "DONE");
+
 	}
 
 	@Override
 	public void run() {
-		threadcounter++;
-		System.out.println(threadcounter);
+		// synchronized (this) {
+		// threadcounter++;
+		// threadcounter.incrementAndGet();
+		System.out.println(threadcounter.incrementAndGet());
+		// }
 	}
 
 }
